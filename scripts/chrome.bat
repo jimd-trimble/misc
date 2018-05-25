843720cf1f55b3f37b4fc1fd3d4e1c3a3584ae43
AM\jdillon <jim_dillon@trimble.com>
2018-05-25 16:16:15 -0600

    Additions & Updates
    Forked the integrated GitCommandLine extension, it's now working
    locally with some extra config.
    
    Other file and script updates.

--- scripts/chrome.bat
+++ scripts/chrome.bat
@@ -1,4 +1,5 @@
 @echo off
 pushd %programfiles(x86)%\Google\Chrome\Application
 start chrome.exe
-start chrome.exe --new-window mail.google.com calendar.google.com drive.google.com intranet.trimble.com webmail.aplus.net jira.trimble.tools/secure/Dashboard.jspa?selectPageId=169600 na33.salesforce.com/home/home.jsp
+start chrome.exe --new-window mail.google.com calendar.google.com drive.google.com  sonarqube intranet.trimble.com webmail.aplus.net jira.trimble.tools/secure/dashboard.jspa?selectpageid=169600 na33.salesforce.com/home/home.jsp ci.trimbleag.team aga-jenkins-master.aga-dev.sandbox.farm:8080
+popd
---
+++ scripts/sonar.bat
@@ -0,0 +1,2 @@
+@echo off
+dev & cd sonarqube/bin/windows-x86-64 & start cmd /k StartSonar
