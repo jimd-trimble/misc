cae674c508848c55f90b5d248843785810423af6
AM\jdillon <jim_dillon@trimble.com>
2018-05-31 00:43:44 -0600

    Housekeeping
    Added a readme for the vpn script so I can share it.
    Added the settings.json file.

--- scripts/vpn.bat
+++ scripts/vpn.bat
@@ -1,6 +1,42 @@
 @echo off
+
+REM ************************READ ME!**************************************************
+REM Create a "scripts" folder somewhere without access restrictions. C:\users\<username>\scripts is fine.
+REM Copy your scripts (even this one) into that directory.
+REM Check the directory's security so that Administrators & System have full control.
+REM 	- You'll want these scripts available for when you "Run As Administrator". 
+REM Copy that path!
+REM Add the path to your system's "Path" variable:
+REM 	- search for "environment variables" in the search box to the right of the Start menu.
+REM 	- click on the "Edit the system environment variables" match.
+REM 	- A "System Properties" window will open. Click the "Environment Variables..." button at the bottom.
+REM 	- This will open another window with user variables on top and system variables on the bottome.
+REM 		- We need the SYSTEM VARIABLES.
+REM 	- Scroll down the system variables panel and double-click on "Path".
+REM 	- Click the "New" button and paste in the scripts directory path.
+REM 	- Click OK, OK, OK.
+REM Now anything in that scripts directory will be available from the terminal no matter which directory you're in!
+REM
+REM Install Viscosity if you haven't. It's a good VPN tool and has command line options.
+REM Add/import your VPN connection profiles. I named mine "primary test" and "primary prod". 
+REM 	- Extra step below if you choose to name yours differently. No big deal. 
+REM In this script:
+REM 	- Set "viscPath" to where you installed Viscosity. No need to put it in your PATH.
+REM 	- Set the "viscPrms" initializer and "SET viscPrms="primary prod" in ":No2".
+REM 	- Save your changes!
+REM
+REM Test it out!
+REM 	- open a new admin terminal (search "cmd", right-click, Run as Admin if you haven't created an admin terminal shortcut).
+REM 		- FYI: whenever changes are made to scripts in your system Path, the terminal must be closed/opened again to refresh.
+REM 	- Make sure you're somewhere in C:\ but NOT in \scripts. You *should* be in C:\Windows\system32.
+REM 	-type "vpn t", you should see a notification that Viscosity is trying to connect.
+REM 	- type "vpd d", you should see a notification that Viscosity is disconnecting.
+REM WHOOOHOOO!
+REM **********************************************************************************
+
 SET	viscPath="C:\Program Files\Viscosity\Viscosity.exe"
 SET viscCmd=connect
+
 SET viscPrms="primary test"
 
 IF "%1" == "t" GOTO No1
