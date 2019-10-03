2788d33bb8b3c112e652e1e7285acd7e156f4acc
AM\\jdillon <jim_dillon@trimble.com>
2019-10-02 18:33:40 -0600

    Update
    It's been a while, pushing changes.

--- MessageEngineTestClient/Program.cs
+++ MessageEngineTestClient/Program.cs
@@ -2,6 +2,8 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Trimble.Ag.BusMessages;
+using Trimble.Ag.DataAccess;
+using Trimble.Ag.DataContracts.Reports;
 using Trimble.Ag.DependencyInjection;
 using Trimble.Ag.MessageConsumers;
 using Trimble.Ag.ServiceBus;
@@ -18,7 +20,10 @@ namespace MessageEngineClient
 			try
 			{
 				//await ImageryFileTest.RunTest(container);
-				await DisplayFileTest.RunTest(container);
+				//await DisplayFileTest.RunTest(container);
+
+				var testAccess = container.Resolve<IAccessDeviceDailyStatistics>();
+				testAccess.UpdateDeviceDailyStatistics();
 				//await engine.ConsumeAsync(message, CancellationToken.None);
 			}
 			catch (Exception e)
